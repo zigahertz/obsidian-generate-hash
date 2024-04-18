ec5b48a13743876703bd165c222662a756640349
zz <dev@zigahertz.com>
2024-04-18 15:33:05 -0400

    removes unused code

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
 import { randomBytes } from 'crypto';
 
 interface HashPluginSettings {
@@ -16,35 +15,20 @@ export default class HashPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('braces', 'Obsidian Hash', (evt: MouseEvent) => {
+		this.addRibbonIcon('braces', 'Obsidian Hash', (evt: MouseEvent) => {
       const view = this.app.workspace.getActiveViewOfType(MarkdownView);
       if (view) {
         this.generateHash(view.editor)
       }
-		});
-		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('obsidian-hash-ribbon-class');
+		})
 
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'generate-hash',
 			name: 'Generate hash',
       editorCallback: (editor: Editor) => this.generateHash(editor)
 		});
 		
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new HashSettingTab(this.app, this));
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
   generateHash(editor: Editor) {
